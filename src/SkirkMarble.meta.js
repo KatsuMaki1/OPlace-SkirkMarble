68599b61095a3f4e0784002b54459a0e1a89a241
K A T S U ! <127791356+KatsuMaki1@users.noreply.github.com>
2026-01-19 14:03:01 +0100

    Rename script from 'Skirk Marble' to 'Midna Marble'

--- src/SkirkMarble.meta.js
+++ src/SkirkMarble.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         Open Skirk Marble
+// @name         Open Midna Marble
 // @namespace    https://github.com/KatsuMaki1/
 // @version      0.10.0
 // @description  A userscript to automate and/or enhance the user experience on openplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with openplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
 // @author       Seris0
 // @license      MPL-2.0
 // @homepageURL  https://github.com/KatsuMaki1/OPlace-SkirkMarble
-// @icon         https://raw.githubusercontent.com/Seris0/Wplace-SkirkMarble/87694688cb85611cbbe9719f852803949289c8af/dist/assets/Favicon.png
+// @icon         https://i.imgur.com/INtw5ix.jpeg
 // @updateURL    https://raw.githubusercontent.com/Seris0/Wplace-SkirkMarble/main/dist/SkirkMarble.user.js
 // @downloadURL  https://raw.githubusercontent.com/Seris0/Wplace-SkirkMarble/main/dist/SkirkMarble.user.js
 // @match        *://*.openplace.live/*
