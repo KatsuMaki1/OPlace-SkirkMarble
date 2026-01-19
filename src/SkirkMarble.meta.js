6f4b2b03575d128b69f528354c773e93d0d26333
K A T S U ! <127791356+KatsuMaki1@users.noreply.github.com>
2026-01-19 13:37:29 +0100

    Update metadata for Open Skirk Marble userscript

--- src/SkirkMarble.meta.js
+++ src/SkirkMarble.meta.js
@@ -2,25 +2,25 @@
 // @name         Open Skirk Marble
 // @namespace    https://github.com/KatsuMaki1/
 // @version      0.10.0
-// @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
+// @description  A userscript to automate and/or enhance the user experience on openplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with openplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
 // @author       Seris0
 // @license      MPL-2.0
-// @homepageURL  https://github.com/Seris0/Wplace-SkirkMarble
+// @homepageURL  https://github.com/KatsuMaki1/OPlace-SkirkMarble
 // @icon         https://raw.githubusercontent.com/Seris0/Wplace-SkirkMarble/87694688cb85611cbbe9719f852803949289c8af/dist/assets/Favicon.png
 // @updateURL    https://raw.githubusercontent.com/Seris0/Wplace-SkirkMarble/main/dist/SkirkMarble.user.js
 // @downloadURL  https://raw.githubusercontent.com/Seris0/Wplace-SkirkMarble/main/dist/SkirkMarble.user.js
-// @match        *://*.wplace.live/*
+// @match        *://*.openplace.live/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM.setValue
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @connect      nominatim.openstreetmap.org
-// @connect      wplace.live
+// @connect      openplace.live
 // @connect      wplace.lol
 // @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-SkirkMarble/87694688cb85611cbbe9719f852803949289c8af/dist/SkirkMarble.user.css
 // ==/UserScript==
 
-// Wplace  --> https://wplace.live
+// Wplace  --> https://openplace.live
 // License --> https://www.mozilla.org/en-US/MPL/2.0/
 
